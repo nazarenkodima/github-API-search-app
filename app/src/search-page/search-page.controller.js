27331eb59c75100a42c3ee2483bfd484fcb724e2
Dmytro Nazarenko <dmamyk@gmail.com>
2017-12-11 01:16:11 +0200

    add pagination & orderBy

--- app/src/search-page/search-page.controller.js
+++ app/src/search-page/search-page.controller.js
@@ -1,4 +1,4 @@
-app.controller('main', function($scope, $http) {
+app.controller('main', function($scope, $http, $log) {
     $scope.repos = $scope.repos || {};
     $scope.repos = {
         data : []
@@ -16,9 +16,30 @@ app.controller('main', function($scope, $http) {
                 $scope.repos.data = response.data;
                 $scope.dataLoaded = true;
                 console.log(response.data);
+
+                //pagination
+                $scope.currentPage = 1;
+                $scope.itemsPerPage = 7;
+                $scope.totalItems = $scope.repos.data.items.length;
+
+                $scope.$watch("currentPage", function() {
+                    setPageData($scope.currentPage);
+                });
+
+
+                function setPageData(page) {
+                    var pagedData = $scope.repos.data.items.slice(
+                        (page - 1) * $scope.itemsPerPage,
+                        page * $scope.itemsPerPage
+                    );
+                    $scope.allRepos = pagedData;
+                }
             })
 
-    }
+    };
+
+
+
 
 
 });
